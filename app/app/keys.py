687c00d9a4165b53d5eb11352e5cfc759eed1c42
Perry <astal01@gmail.com>
2017-11-06 10:19:51 -0800

    switching from KMS to parameter store

--- app/app/keys.py
+++ app/app/keys.py
@@ -1,41 +1,10 @@
 import os
 import boto3
 ENVIRONMENT = os.environ.get("ENVIRONMENT", 'local')
-DB_PASSWORD = None
-FACEBOOK_TOKEN = None
-SECRET_KEY = None
-parameters = dict()
 
-
-if os.environ.get('ENCRYPTED_FACEBOOK_TOKEN'):
-    parameters[os.environ.get('ENCRYPTED_FACEBOOK_TOKEN')] = 'FACEBOOK_TOKEN'
-else:
-    FACEBOOK_TOKEN = os.environ.get('FACEBOOK_TOKEN')
-
-if os.environ.get('ENCRYPTED_DJANGO_SECRET_KEY'):
-    parameters[os.environ.get('ENCRYPTED_DJANGO_SECRET_KEY')] = 'SECRET_KEY'
-else:
-    SECRET_KEY = 'kzv$s=4j$y5hkx4$9kai799(kf$ac^rix=t_0b149*wx!iit+t'
-
-if os.environ.get('ENCRYPTED_CRAFTY_DB_PASSWORD'):
-    parameters[os.environ.get('ENCRYPTED_CRAFTY_DB_PASSWORD')] = 'DB_PASSWORD'
-else:
-    DB_PASSWORD = 'test'
-
-ssm = boto3.client('ssm')
-
-stored_parameters = ssm.get_parameters(
-    Names=parameters.keys(),
-    WithDecryption=True
-)
-
-for param in stored_parameters.get('Parameters', list()):
-    if parameters[param['Name']] == 'FACEBOOK_TOKEN':
-        FACEBOOK_TOKEN = param['Value']
-    elif parameters[param['Name']] == 'SECRET_KEY':
-        SECRET_KEY = param['Value']
-    elif parameters[param['Name']] == 'DB_PASSWORD':
-        DB_PASSWORD = param['Value']
+FACEBOOK_TOKEN = os.environ.get('ENCRYPTED_FACEBOOK_TOKEN')
+DB_PASSWORD = os.environ.get('ENCRYPTED_CRAFTY_DB_PASSWORD', 'test')
+SECRET_KEY = os.environ.get('ENCRYPTED_DJANGO_SECRET_KEY', 'kzv$s=4j$y5hkx4$9kai799(kf$ac^rix=t_0b149*wx!iit+t')
 
 BUCKETS = dict(
     default=os.environ.get('CRAFTY_DEFAULT_BUCKET', "craftybot"),
