7d9b1e199c8b76b3cdac6cb03ee9f49b64864d23
Perry <astal01@gmail.com>
2018-01-13 11:13:19 -0800

    switched to eventlet gunicorn workers

--- deployment/conf/gunicorn_settings.py
+++ deployment/conf/gunicorn_settings.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "unix:django_app.sock"
-#worker_class = 'aiohttp'
+worker_class = 'eventlet'
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '-'
 errorlog = '-'
